85d3701903290204ee748e82e0148394b8cce6e7
dmitry.usachev <dmitry.usachev@indeed-id.com>
2024-10-02 21:06:44 +0300

    Day3 HW task2

--- HW_Day_3/task2/m5_t1_hw.go
+++ HW_Day_3/task2/m5_t1_hw.go
@@ -32,4 +32,97 @@
 */
 package main
 
-// Ваш код
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"unicode/utf8"
+)
+
+var (
+	digits string = "0123456789"
+	lowercase string = "abcdefghijklmnopqrstuvwxyz"
+	uppercase string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	special string = "_!@#$%^&"
+)
+
+func main() {
+	outer:
+	for i := 0; i < 5; i++ {
+		fmt.Println("Введите пароль:")
+		str, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		result := checkPassword(str)
+		if result == true {
+			fmt.Println("*** Пароль правильный и он принят ***")
+			break outer
+		} else if i != 4 {
+			fmt.Printf("*** Осталось %v попыток ***\n", 4 - i)
+		} else {
+			fmt.Println("*** Попытки закончились запустите программу снова ***")
+			break outer
+		}
+	}
+}
+
+// Проверка пароля на соответствие парольной политки
+func checkPassword(pass string) bool {
+	status := 0
+	if checkPasswordChars(pass, digits) {
+		status += 1
+	} else {
+		fmt.Println("В пароле нет цифр!")
+	}
+
+	if checkPasswordChars(pass, lowercase) {
+		status += 1
+	} else {
+		fmt.Println("В пароле нет маленьких букв!")
+	}
+
+	if checkPasswordChars(pass, uppercase) {
+		status += 1
+	} else {
+		fmt.Println("В пароле нет больших букв!")
+	}
+
+	if checkPasswordChars(pass, special) {
+		status += 1
+	} else {
+		fmt.Println("В пароле нет спец символов!")
+	}
+	if checkLenPassword(pass) {
+		status += 1
+	} else {
+		fmt.Println("Длина пароля должна быть от 8(мин) до 15(макс) символов")
+	}
+
+	if status == 5 {
+		return true
+	} else {
+		return false
+	}
+}
+
+// Проверка пароля на наличее символла из переданного набора
+func checkPasswordChars(password string, chars string) bool {
+	flag := false
+	outer:
+	for _, pass := range password {
+		for _, char := range chars {
+			if string(char) == string(pass) {
+				flag = true
+				break outer
+			}
+		}
+	}
+	return flag
+}
+
+// Проверка пароля на его длинну
+func checkLenPassword(pass string) bool {
+	flag := false
+		if  utf8.RuneCountInString(pass) >= 8 && utf8.RuneCountInString(pass) <= 15 {
+			flag = true
+		}
+	return flag
+}
